997e36ca6586e3e44cda3ceef6443190dce5426c
jamblet <fumba.yolokazi@gmail.com>
2023-09-19 05:25:52 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <p>
+        This app was created by{" "}
+        <a href="https://github.com/Yolokazifumba">Yolokazi Fumba</a> and it is
+        an
+        <a href="https://github.com/Yolokazifumba/react-weather-app">
+          {" "}
+          open-source code
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default App;
